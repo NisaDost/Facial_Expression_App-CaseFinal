d2bf0b8a48a3750962c005ff72fcbcd9dd55f54a
Nisa Dost <200706004@st.maltepe.edu.tr>
2023-12-15 13:44:43 +0300

    box are gone but captures image

--- app/src/main/java/com/example/imagepro/CameraActivity.java
+++ app/src/main/java/com/example/imagepro/CameraActivity.java
@@ -1,18 +1,22 @@
 package com.example.imagepro;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -20,121 +24,158 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import java.io.File;
 import java.io.IOException;
 
-public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
-    private static final String TAG="MainActivity";
+public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+    private static final String TAG = "CameraActivity";
+    private static final String IMAGE_FOLDER_PATH = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES) + "/ImagePro/";
 
     private Mat mRgba;
     private Mat mGray;
     private CameraBridgeViewBase mOpenCvCameraView;
-    // call java class
     private facialExpressionRecognition facialExpressionRecognition;
     private Button captureButton;
 
-    private BaseLoaderCallback mLoaderCallback =new BaseLoaderCallback(this) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
-                case LoaderCallbackInterface
-                        .SUCCESS:{
-                    Log.i(TAG,"OpenCv Is loaded");
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCv Is loaded");
                     mOpenCvCameraView.enableView();
                 }
-                default:
-                {
+                default: {
                     super.onManagerConnected(status);
-
                 }
                 break;
             }
         }
     };
 
-    public CameraActivity(){
-        Log.i(TAG,"Instantiated new "+this.getClass());
+    public CameraActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        int MY_PERMISSIONS_REQUEST_CAMERA=0;
-        // if camera permission is not given it will ask for it on device
+        // Check for camera permission
         if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(CameraActivity.this, new String[] {Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
         }
 
         setContentView(R.layout.activity_camera);
 
-        mOpenCvCameraView=(CameraBridgeViewBase) findViewById(R.id.frame_Surface);
+        // Ensure the folder for saving images exists
+        File folder = new File(IMAGE_FOLDER_PATH);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        captureButton = findViewById(R.id.capture_button);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                captureImage();
+            }
+        });
+
+        mOpenCvCameraView = findViewById(R.id.frame_Surface);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
-        // this will load cascade classifier and model
-        // this only happen one time when you start CameraActivity
-        try{
-            // input size of model is 48
-            int inputSize=48;
-            facialExpressionRecognition=new facialExpressionRecognition(getAssets(),CameraActivity.this,
-                    "model300.tflite",inputSize);
-        }
-        catch (IOException e){
+
+        try {
+            int inputSize = 48;
+            facialExpressionRecognition = new facialExpressionRecognition(getAssets(), CameraActivity.this,
+                    "model300.tflite", inputSize);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
+
+    private void captureImage() {
+        // Ensure the folder for saving images exists
+        File folder = new File(IMAGE_FOLDER_PATH);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+
+        // Create a filename for the image
+        String fileName = IMAGE_FOLDER_PATH + "captured_image.jpg";
+
+        // Capture the current frame from the camera view
+        Mat rgbaImage = mRgba.clone();  // Make a copy to avoid modifying the displayed frame
+
+        // Save the captured image to the specified file
+        Imgcodecs.imwrite(fileName, rgbaImage);
+
+        // Notify the media scanner to recognize the new image
+        MediaScannerConnection.scanFile(this, new String[]{fileName}, null, new MediaScannerConnection.OnScanCompletedListener() {
+            @Override
+            public void onScanCompleted(String path, Uri uri) {
+                Log.i(TAG, "Image scanned: " + path);
+            }
+        });
+
+        // Implement further processing with the captured image data
+        // For example, you can save it to a file or send it to a server
+
+        Toast.makeText(CameraActivity.this, "Image Captured and Saved!", Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (OpenCVLoader.initDebug()){
-            //if load success
-            Log.d(TAG,"Opencv initialization is done");
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Opencv initialization is done");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-        else{
-            //if not loaded
-            Log.d(TAG,"Opencv is not loaded. try again");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this,mLoaderCallback);
+        } else {
+            Log.d(TAG, "Opencv is not loaded. try again");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0, this, mLoaderCallback);
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mOpenCvCameraView !=null){
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
     }
 
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
-        if(mOpenCvCameraView !=null){
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
         }
-
     }
 
-    public void onCameraViewStarted(int width ,int height){
-        mRgba=new Mat(height,width, CvType.CV_8UC4);
-        mGray =new Mat(height,width,CvType.CV_8UC1);
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
     }
-    public void onCameraViewStopped(){
+
+    public void onCameraViewStopped() {
         mRgba.release();
     }
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
-        mRgba=inputFrame.rgba();
-        mGray=inputFrame.gray();
-        //output                                         input
-        mRgba=facialExpressionRecognition.recognizeImage(mRgba);
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+        mRgba = facialExpressionRecognition.recognizeImage(mRgba);
         return mRgba;
-
     }
-
 }
